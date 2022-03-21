d8add757bedb12ca68fe19f4421aceeaabd16c56
Muhammad <samsmusa@outlook.com>
2022-03-21 12:44:20 +0600

    3-21-2022 12:44

---
+++ doormat.py
@@ -0,0 +1,15 @@
+
+import sys
+row, col = map(int, sys.stdin.readline().split (" "))
+text = "WELCOME"
+
+# top bar 
+for j in range(row//2,0,-1):
+    print((3*j)*"-"+".|."*(row//2+1-j)+".|."*(row//2-j)+(3*j)*"-")
+    
+# middle bar 
+print((col-len(text))//2 * "-"+text+(col-len(text))//2 * "-")
+    
+# bottom bar 
+for j in range(1,row//2+1):
+    print((3*j)*"-"+".|."*(row//2+1-j)+".|."*(row//2-j)+(3*j)*"-")
